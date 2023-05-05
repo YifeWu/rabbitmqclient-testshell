95a2e83841aeb42e7c6c0d5f1001130c417fc356
Yifei Wu <YWu1@slb.com>
2023-05-05 18:14:05 -0500

    Add test shell

--- main.go
+++ main.go
@@ -4,12 +4,45 @@ import (
 	"log"
 
 	rabbitmqclient "dev.azure.com/slb1-swt/wireline-automation/_git/dev-pdp-maxwell-interface.git/rabbitmqclient"
-	yifeilogger "github.com/YifeWu/gologger"
 )
 
+type MySubscriber struct {
+	Topic string //Todo: This property is not needed, to be removed.
+}
+
+/*
+Called the received message for the topic
+*/
+func (sub MySubscriber) HandleMessage(body []byte, topic string, replyToTopic string) {
+	log.Printf(" [x] %s", topic)
+}
+
 func main() {
-	log.Println("hello")
-	yifeilogger.LogInfo("github module test")
-	// loggershell.LogInfo("This is very good news!")
-	rabbitmqclient.LogRabbitMQ("This is fantascis!")
+
+	log.Printf("Hello PDP Express")
+	rabbitmqclient.Greeting()
+
+	connMngr := rabbitmqclient.CreateConnectionManager()
+
+	// sub := MySubscriber{Topic: "maxwell.job.status.state.v1"}
+	// connMngr.SubscribeExchange("pubsub", "maxwell.job.status.state.v1", sub)
+
+	testPubSub := true
+	if testPubSub {
+		// Subscribe to Maxwell pub/sub topic, then replish to PDP express pub/sub topic
+		repubSub := rabbitmqclient.RepublishSubscriber{RepublishTopic: "pdp.express.depth.tension.state.v1", ConnManager: connMngr}
+		connMngr.Subscribe("maxwell.job.status.state.v1", repubSub)
+	}
+
+	testRpc := false
+	if testRpc {
+		// Subscribe to Maxwell RPC topic, then replish to PDP express RPC topic
+		repubRpc := rabbitmqclient.RepublishSubscriber{RepublishTopic: "pdp.express.job_summary.get.v1", ConnManager: connMngr}
+		msg := rabbitmqclient.BuildPayload()
+		connMngr.PublishRequest("maxwell.job_summary.get.v1", msg, repubRpc)
+	}
+
+	var forever chan struct{}
+	log.Printf(" [*] Waiting ")
+	<-forever
 }
